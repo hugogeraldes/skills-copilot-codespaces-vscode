a574010d4369cf80340420491a16c4ca5b0f5e24
hugogeraldes <hnunes@microsoft.com>
2023-08-13 11:29:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,28 @@
 //create web server
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
-//connect to database
-var mongoose = require('mongoose');
+const express = require('express')
+
+//create application
+const app = express()
+
+//create a port
+const port = 3000
+
+//create a home page
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+//create a comments page
+app.get('/comments', (req, res) => {
+    res.send('This is the comments page')
+})
+
+//create a comments page
+app.get('/comments/:id', (req, res) => {
+    res.send('This is the comments page for ' + req.params.id)
+})
+
+//start the server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
